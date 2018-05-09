9d0d5de9628cab448f07a9c92ced79b8f4489d2d
Khalid Asad <khalidasad@Khalids-MacBook-Pro.local>
2018-05-09 16:20:50 -0400

    Fixed second controller view - now expandable list view is working.

--- portfolio/SecondViewController.swift
+++ portfolio/SecondViewController.swift
@@ -12,11 +12,11 @@ import UIKit
 
 class SecondViewController: UITableViewController {
 
-    //var jobData: [JobData?]?
+    var jobData: [JobData?]?
     
     override func viewDidLoad() {
-        //jobData = getData()
-        //tableView.rowHeight = UITableViewAutomaticDimension
+        jobData = getData()
+        tableView.rowHeight = UITableViewAutomaticDimension
         
         super.viewDidLoad()
 
@@ -26,11 +26,11 @@ class SecondViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-    /*
+    
     private func getData() -> [JobData?] {
         let data: [JobData?] = []
         
-        let rogersDesc = [JobDescData(jobDesc: "Here is the job description for rogers job."), JobDescData(jobDesc: "more job description for rogers job.")]
+        let rogersDesc = [JobDescData(jobDesc: "Rotation 1: Production Support (June 2017 - October 2017)"), JobDescData(jobDesc: "• Automated the strenuous verification and hourly updating of IT application statuses for 5 systems effectively during the critical days period saving 10,000 hours of overtime for 3 teams"), JobDescData(jobDesc: "• Developed an automated provisioning tool for an IT application that improved efficiency by 98%"), JobDescData(jobDesc: "• Intuitively designed a PowerShell script to retrieve employee fields via Employee IDs bypassing LDAP restrictions"), JobDescData(jobDesc: "\nRotation 2: Business Systems Analyst (October 2017 - February 2018)"), JobDescData(jobDesc: "• Built a DDD, running PCR, use cases and SDS draft in a project that generated $0.5 million monthly"), JobDescData(jobDesc: "• Automated Actuals vs. Forecasting costs for a massive mandatory project saving 8 hours a week"), JobDescData(jobDesc: "\nRotation 3: IT Security (February 2018 - March 2018)"), JobDescData(jobDesc: "• Extensively researched Governance, Risk, and Compliance and participated in the first stage of Vendor Selection Process"), JobDescData(jobDesc: "\nRotation 4: Web Developer (March 2018 - June 2018)"), JobDescData(jobDesc: "• Increased the performance by 10% of a handful of broadcast websites running on WordPress by performing upgrades, introducing new plugins, and optimizing PHP and CSS code"),]
         let rogers = JobData(companyImage: "rogers", jobTitle: "IT New Grad", dates: "june-present", jobDescription: rogersDesc)
         
         let n8idDesc = [JobDescData(jobDesc: "Here is the job description for n8id job."), JobDescData(jobDesc: "more job description for n8id job.")]
@@ -45,13 +45,6 @@ class SecondViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -129,7 +122,7 @@ class SecondViewController: UITableViewController {
     
     private func getParentCellIndex(expansionIndex: Int) -> Int{
         var selectedCell: JobData?
-        var selectedCellIndex: expansionIndex
+        var selectedCellIndex = expansionIndex
         
         while(selectedCell == nil && selectedCellIndex >= 0){
             selectedCellIndex -= 1
@@ -137,7 +130,7 @@ class SecondViewController: UITableViewController {
         }
         
         return selectedCellIndex
-    }*/
+    }
     
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
