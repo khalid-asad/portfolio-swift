35e9439a265af86cab0f7252540327312c3d367d
Khalid Asad <khalidasad@Khalids-MacBook-Pro.local>
2018-05-08 23:59:47 -0400

    Completed first page that contains links - everything working as designed

---
+++ portfolio/CustomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCell.swift
+//  portfolio
+//
+//  Created by Khalid Asad on 2018-05-08.
+//  Copyright © 2018 khalidtest. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageCell: UIButton!
+    @IBOutlet weak var labelCell: UILabel!
+    
+}
