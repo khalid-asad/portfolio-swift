fd99af1b35d8a22c53a3cc754468d31050c32e88
Khalid Asad <khalid.asad@rci.rogers.com>
2019-07-09 22:36:29 -0400

    added my PlatformCommon framework

---
+++ Carthage/Checkouts/PlatformCommon/PlatformCommon/PlatformCommon.h
@@ -0,0 +1,19 @@
+//
+//  PlatformCommon.h
+//  PlatformCommon
+//
+//  Created by Khalid Asad on 2019-06-24.
+//  Copyright © 2019 Khalid Asad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlatformCommon.
+FOUNDATION_EXPORT double PlatformCommonVersionNumber;
+
+//! Project version string for PlatformCommon.
+FOUNDATION_EXPORT const unsigned char PlatformCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlatformCommon/PublicHeader.h>
+
+
